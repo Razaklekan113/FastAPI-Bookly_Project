387af6178e205f9abc79eca6c093952de33b65b9
Razaklekan113 <razaklekan11@gmail.com>
2024-12-11 14:52:17 +0100

    testing how to get the headers

--- main.py
+++ main.py
@@ -26,10 +26,16 @@ async def create_book(book_data:BookCreateModel):
 
 @app.get("/get_headers")
 async def get_headers(
-    accept: str = Header(None)
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None)
 ):
     request_headers = {}
 
     request_headers["Accept"] = accept
+    request_headers["CONTENT-TYPE"] = content_type
+    request_headers["User-Agent"] = user_agent
+    request_headers["Host"] = host
 
     return request_headers
