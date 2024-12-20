bcb72db988e7de980ab909ae7b924f06c3cb4f9c
Razaklekan113 <razaklekan11@gmail.com>
2024-12-20 22:19:35 +0100

    updated

--- main.py
+++ main.py
@@ -65,13 +65,20 @@ books = [
 ]
 
 class Book(BaseModel):
-        id: int
-        title: str
-        author: str
-        publisher:str
-        published_date: str
-        page_count: int
-        language: str
+    id: int
+    title: str
+    author: str
+    publisher:str
+    published_date: str
+    page_count: int
+    language: str
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher:str
+    page_count: int
+    language: str
 
 
 @app.get("/books", response_model=List[Book])
@@ -93,9 +100,17 @@ async def get_a_book(book_id:int) -> dict:
             return book
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@app.post("/books/{book_id}")
-async def update_book(book_id:int) -> dict:
-    pass
+@app.patch("/books/{book_id}")
+async def update_book(book_id:int, book_update_data:BookUpdateModel) -> dict:
+    for book in books :
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["author"] = book_update_data.author
+            book["publisher"] = book_update_data.publisher
+            book["page_count"] = book_update_data.page_count
+            book["language"] = book_update_data.language
+            return book
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 @app.post("/books/{book_id}")
 async def delete_book(book_id:int) -> dict:
