043367dcaf3d9ccde177dd2a163635dde9e03d82
Razaklekan113 <razaklekan11@gmail.com>
2024-12-25 21:45:43 +0100

    added a router for the delete method

--- main.py
+++ main.py
@@ -112,6 +112,10 @@ async def update_book(book_id:int, book_update_data:BookUpdateModel) -> dict:
             return book
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@app.post("/books/{book_id}")
+@app.post("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id:int) -> dict:
-    pass
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
