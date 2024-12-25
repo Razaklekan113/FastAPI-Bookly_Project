ccf9aff66f7080e68105702bbc3dec84a516aa34
Razaklekan113 <razaklekan11@gmail.com>
2024-12-25 21:52:14 +0100

    got adjusted

--- main.py
+++ main.py
@@ -113,9 +113,9 @@ async def update_book(book_id:int, book_update_data:BookUpdateModel) -> dict:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id:int) -> dict:
+async def delete_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
             books.remove(book)
-            return {}
+            return 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
