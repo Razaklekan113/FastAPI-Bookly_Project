9ff9f148e6bade1cacb45de8cad26869a4ddfb39
Razaklekan113 <razaklekan11@gmail.com>
2025-01-10 17:23:00 +0100

    added a tag

--- src/__init__.py
+++ src/__init__.py
@@ -9,4 +9,4 @@ app = FastAPI(
     description="A simple API for managing books",
     version=version
 )
-app.include_router(book_router, prefix=f"api/{version}/books")
+app.include_router(book_router, prefix=f"api/{version}/books", tags=["books"])
