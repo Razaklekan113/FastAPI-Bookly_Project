8b092fe6b7736268ea888272829482ba6198b164
Razaklekan113 <razaklekan11@gmail.com>
2025-01-10 17:20:36 +0100

    modified the app

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,8 @@ from src.books.routes import book_router
 version = "v1"
 
 app = FastAPI(
+    title="Bookly",
+    description="A simple API for managing books",
     version=version
 )
 app.include_router(book_router, prefix=f"api/{version}/books")
